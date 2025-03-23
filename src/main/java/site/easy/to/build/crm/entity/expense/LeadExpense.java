34f763980a77f059eb1267cbfbdf000e1eb9ea34
sandratraniaina <sandratrarafa@gmail.com>
2025-03-23 13:23:08 +0300

    feat: Create LeadExpense entity

---
+++ src/main/java/site/easy/to/build/crm/entity/expense/LeadExpense.java
@@ -0,0 +1,47 @@
+package site.easy.to.build.crm.entity.expense;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import site.easy.to.build.crm.entity.Lead;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import org.springframework.security.core.userdetails.User;
+
+@Entity
+@Table(name = "lead_expense")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class LeadExpense {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "amount", nullable = false)
+    @DecimalMin(value = "0", message = "Amount must be greater than 0")
+    private BigDecimal amount;
+
+    @Column(name = "description", nullable = true)
+    private String description;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @Column(name = "expense_date", nullable = false)
+    private LocalDate expenseDate = LocalDate.now();
+
+    @ManyToOne
+    @JoinColumn(name = "created_by", nullable = false)
+    private User createdBy;
+
+    @ManyToOne
+    @JoinColumn(name = "lead_id", nullable = false)
+    private Lead lead;
+}
