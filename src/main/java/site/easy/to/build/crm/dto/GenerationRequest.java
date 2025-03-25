a1e1fb02ff62bd3191b8d9e34e39c1826c8798be
sandratraniaina <sandratrarafa@gmail.com>
2025-03-25 23:40:54 +0300

    feat: Create GenerationRequest class

--- src/main/java/site/easy/to/build/crm/dto/GenerationRequest.java
+++ src/main/java/site/easy/to/build/crm/dto/GenerationRequest.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-class GenerationRequest {
+public class GenerationRequest {
     private int customerCount;
     private int budgetCount;
     private int ticketExpenseCount;
